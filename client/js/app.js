acd6c558ce16e19bba4b346c34cb50d9f28e1b26
WolfgangHall <johnalexander.w.hall@gmail.com>
2016-03-28 01:25:01 -0400

    removed two periods from file routing that leads to partials

--- client/js/app.js
+++ client/js/app.js
@@ -6,25 +6,25 @@ angular.module('portfolioApp', [
     $stateProvider
       .state('home', {
         url: '/home',
-        templateUrl: '../partials/partial-home.html',
+        templateUrl: '/partials/partial-home.html',
         controller: 'homeController'
       })
 
       .state('about', {
         url: '/about',
-        templateUrl: '../partials/partial-about.html',
+        templateUrl: '/partials/partial-about.html',
         controller: 'aboutController'
       })
 
       .state('projects', {
         url: '/projects',
-        templateUrl: '../partials/partial-projects.html',
+        templateUrl: '/partials/partial-projects.html',
         controller: 'projectsController'
       })
 
       .state('contact', {
         url: '/contact',
-        templateUrl: '../partials/partial-contact.html',
+        templateUrl: '/partials/partial-contact.html',
         controller: 'contactController as contact'
       })
 
